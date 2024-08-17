14c377dd67d3e4baf4271f395e517535618e2a8c
IshanFTW <ishanjindal2003@gmail.com>
2024-08-17 15:29:41 +0530

    api test successfull

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react"
+
+interface recipeType{
+  name: string,
+  instructions: string,
+  ingredients: string,
+}
+
 function App() {
+  const [recipe, setRecipe] = useState<recipeType |null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  async function fetchRecipe(name: string) {
+    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+    if(response){
+      setRecipe({name: response.data.meals[0].strMeal , instructions: response.data.meals[0].strInstructions, ingredients: response.data.meals[0].strIngredient1})
+    }
+  }
+
+  const handleSearch = () => {
+    const searchValue = inputRef.current?.value;
+    if(searchValue){
+      fetchRecipe(searchValue);
+    }
+  }
+
+  
   return (
-    <div>app</div>
+    <div>
+      <input type="text" name="" id="" placeholder="Recipe" ref = {inputRef} />
+      <button onClick={handleSearch}>Search</button>
+      <div>{recipe?.name}</div>
+      <div>{recipe?.instructions}</div>
+      <div>{recipe?.ingredients}</div>
+    </div>
   )
 }
 
