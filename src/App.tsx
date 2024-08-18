4bea99388546cd33dfa7a1275f0d8d94cec9ff70
IshanFTW <ishanjindal2003@gmail.com>
2024-08-18 14:45:19 +0530

    list all the recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,62 @@
 import axios from "axios";
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 
-interface recipeType{
+interface RecipeType{
+  id: string,
   name: string,
   instructions: string,
-  ingredients: string,
+  ingredients: string[],
 }
 
 function App() {
-  const [recipe, setRecipe] = useState<recipeType |null>(null);
+  const [recipes, setRecipes] = useState<RecipeType[]>([]);
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   async function fetchRecipe(name: string) {
     const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
-    if(response){
-      setRecipe({name: response.data.meals[0].strMeal , instructions: response.data.meals[0].strInstructions, ingredients: response.data.meals[0].strIngredient1})
+    console.log(response);
+    const meals = response.data.meals || [];
+    
+    const fetchRecipes = meals.map((meal: any) => {
+      const ingredients = [];
+      for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        if (ingredient) ingredients.push(ingredient);
     }
-  }
+    return {
+      id: meal.idMeal,
+      name: meal.strMeal,
+      instructions: meal.strInstructions,
+      ingredients,
+    };
+  });
 
-  const handleSearch = () => {
-    const searchValue = inputRef.current?.value;
-    if(searchValue){
-      fetchRecipe(searchValue);
+      setRecipes(fetchRecipes);
     }
-  }
-
+    const handleSearch = () => {
+      const searchValue = inputRef.current?.value;
+      if(searchValue){
+        fetchRecipe(searchValue);
+      }
+    }
   
   return (
     <div>
-      <input type="text" name="" id="" placeholder="Recipe" ref = {inputRef} />
+      <input type="text" placeholder="Recipe" ref={inputRef} />
       <button onClick={handleSearch}>Search</button>
-      <div>{recipe?.name}</div>
-      <div>{recipe?.instructions}</div>
-      <div>{recipe?.ingredients}</div>
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <p>{recipe.instructions}</p>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
